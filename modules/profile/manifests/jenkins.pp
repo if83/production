0ef69c385538e840b3295be680959cf7ffc0d5f9
oleksdiam <oleksdiam@yahoo.com>
2018-04-04 11:49:04 +0300

    add rsyslog client to profile

--- modules/profile/manifests/jenkins.pp
+++ modules/profile/manifests/jenkins.pp
@@ -1,12 +1,13 @@
 class profile::jenkins::master {
-  $dports        = ['8080']
-  $log_proto     = 'tcp'
-  $log_port      = '601'
-  $log_serv      = '192.168.56.10'
-  $user_host     = 'localhost'
-  $java_mode     = 'jdk'
-  $sonar_version = '6.7.2'
+  $dports          = ['8080']
+  $log_proto       = 'tcp'
+  $log_port        = '601'
+  $log_serv        = '192.168.56.10'
+  $user_host       = 'localhost'
+  $java_mode       = 'jdk'
+  $jenkins_version = '6.7.2'
 
+  # set of apps needs to be logged
   $jenkins = {
     log_name => '/var/log/jenkins/*.log',
     app_name => 'jenkins',
@@ -19,11 +20,11 @@ class profile::jenkins::master {
   }
   include jenkins
 
-  # base::ssh_user { $ssh_user:
-  #   ssh_user     => $ssh_user,
-  #   ssh_group    => $ssh_group,
-  #   ssh_password => $ssh_password,
-  # }
+  base::ssh_user { $ssh_user:
+    ssh_user     => $ssh_user,
+    ssh_group    => $ssh_group,
+    ssh_password => $ssh_password,
+  }
   firewall::openport {'jenkins':
     dports       => $dports,
   }
