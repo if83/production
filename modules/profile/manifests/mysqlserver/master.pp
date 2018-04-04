fc77bacad3ab6bb08f8154a4e16e08e0dde5b238
turnopil <turnopil@gmail.com>
2018-04-04 23:02:06 +0300

    Removed comas in mysql profiles

--- modules/profile/manifests/mysqlserver/master.pp
+++ modules/profile/manifests/mysqlserver/master.pp
@@ -1,13 +1,13 @@
 class profile::mysqlserver::master {
-  $mysql_root_password = "a8+?treAvpDa", # My root password
-  $mysql_distro        = "community", # Distributive name. For now community only 
-  $mysql_version       = "5.7", # Provide the Mysql Version. You can set 5.7 or 5.6
-  $mysql_serverid      = "1", # Provide the Server ID = 1.2.3.... etc
-  $port                = ['3306'], # can also be defined under my.cnf
-  $bind_address        = "0.0.0.0",  # can also be defined under my.cnf
-  $is_master           = true, # True if the node is master
-  $replica_user        = "replication", # For master, what is the replication account
-  $replica_password    = "Pr0m3Teus!", # Replication User password
+  $mysql_root_password = "a8+?treAvpDa" # My root password
+  $mysql_distro        = "community" # Distributive name. For now community only 
+  $mysql_version       = "5.7" # Provide the Mysql Version. You can set 5.7 or 5.6
+  $mysql_serverid      = "1" # Provide the Server ID = 1.2.3.... etc
+  $port                = ['3306'] # can also be defined under my.cnf
+  $bind_address        = "0.0.0.0"  # can also be defined under my.cnf
+  $is_master           = true # True if the node is master
+  $replica_user        = "replication" # For master, what is the replication account
+  $replica_password    = "Pr0m3Teus!" # Replication User password
 
 
 firewall::openport {'mysqlmaster':
