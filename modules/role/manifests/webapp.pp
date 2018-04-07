41cac3184d90617dc383480722b9f3ca73b4c01e
Bohdan Deputat <36072762+bdeputat@users.noreply.github.com>
2018-04-07 15:58:13 +0300

    Update webapp.pp

--- modules/role/manifests/webapp.pp
+++ modules/role/manifests/webapp.pp
@@ -1,8 +1,3 @@
 class role::webapp {
     include profile::basenode
-<<<<<<< HEAD
     include profile::webapp::tomcat
-=======
-    include profile::tomcat::tomcat
->>>>>>> 9a76874044cfff6a40ff47b58cf74dfcfe25fbf1
-}
