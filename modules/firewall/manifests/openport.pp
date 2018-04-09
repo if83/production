96cc603c7bbaff946d7272308a49eeb865c2fd43
oleksdiam <oleksdiam@yahoo.com>
2018-04-09 10:13:10 +0300

    ability to work with different protocols is added

--- modules/firewall/manifests/openport.pp
+++ modules/firewall/manifests/openport.pp
@@ -21,7 +21,7 @@ define firewall::openport (
   require firewall
 
   $dports.each |String $dport| {
-    exec { "firewall-cmd-${dport}-{$dproto}":
+    exec { "firewall-cmd-${dport}-${dproto}":
       command => "firewall-cmd --zone=public --add-port=${dport}/${dproto} --permanent",
       path    => "/usr/bin/",
       before  => Exec["firewall-reload${title}"],
