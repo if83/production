004399ebe9213a3dce8d6e454ca3fae63802643f
turnopil <turnopil@gmail.com>
2018-04-10 23:51:48 +0300

    Added profile dump for bugtrckr database

---
+++ modules/profile/manifests/mysqlserver/dump.pp
@@ -0,0 +1,21 @@
+# Class: profile::mysqlserver::dump
+#
+#
+class profile::mysqlserver::dump {
+
+$root_pwd = $mysql::mysql_root_password
+file { 'data-dump.sql':
+  ensure => present,
+  path   => '/tmp/data-dump.sql',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
+  source => 'puppet:///modules/mysql/data-dump.sql',
+}
+
+exec { 'mysql_import_tables':
+  path    => '/usr/bin:/usr/sbin:/bin',
+  command => "mysql -u root -p'${root_pwd}' bugtrckr < /tmp/data-dump.sql",
+  unless  => "mysql -u root -p'${root_pwd}' -e \"SELECT * FROM IssueComment;\"",
+  require => File['data-dump.sql'],
+}
