4268ea8b39c32877a30513667bee9b50f58bbc8a
oleksdiam <36075222+oleksdiam@users.noreply.github.com>
2018-04-26 16:50:25 +0300

    added parser of host list for passing logs from rsyslog to ELK (#27)

--- modules/profile/manifests/rsyslog.pp
+++ modules/profile/manifests/rsyslog.pp
@@ -1,6 +1,9 @@
 class profile::rsyslog {
   $dports        = ['601']
 
+  $node_s    = generate("/bin/bash", "-c", "/bin/awk 'NF > 0 {print \$1}' /etc/puppetlabs/code/environments/production/modules/profile/templates/fwd_zone_list.erb")
+  $node_list = split($node_s, "\n")
+
   class { 'rsyslog::server':
     log_proto   => 'tcp',
     log_port    => '601',
@@ -13,7 +16,8 @@ class profile::rsyslog {
     elk_server => 'elk',
     elkport    => '2000',
     elkproto   => 'tcp',
-    node_list  => [dns, sonar, web1, web2, web3, jenkins, zabbix, db, dbslave, balanser, gitlab, repo],
+    node_list  => $node_list,
+    # node_list  => [dns, sonar, web1, web2, web3, jenkins, zabbix, db, dbslave, balanser, gitlab, repo],
   }
 
   firewall::openport {'rsyslog':
