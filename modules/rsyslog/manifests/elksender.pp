3463926d43fe5055d0b3bff6d7519b7bd2c98444
oleksdiam <oleksdiam@yahoo.com>
2018-04-26 15:36:24 +0300

    added parser of host list for passing logs from rsyslog to ELK

--- modules/rsyslog/manifests/elksender.pp
+++ modules/rsyslog/manifests/elksender.pp
@@ -7,14 +7,7 @@ class rsyslog::elksender (
   $elkproto   = 'tcp',
   $node_list  = [dns],
 ){
-  # $_file_contents = template('profile/fwd_zone_list.erb')
-  # $_file_lines    = split($_file_contents, "\n")
-  # $node_list = regsubst($_file_lines,'^(\w)\s(*)$','\1')
-  # $node_s = generate("/bin/bash", "-c", "/bin/awk 'NF > 0 {print \$1}' /etc/puppetlabs/code/environments/production/modules/profile/templates/fwd_zone_list.erb")
-  # $node_list = generate("/bin/bash", "-c", "echo '['\$(cat /etc/puppetlabs/code/environments/production/modules/profile/templates/fwd_zone_list.erb | cut -d ' ' -f1 |tr '\n' ',')']'")
-  # $node_list = '['$node_l']'    #\${dnsfile}                             
-  # $node_list.each |$node| {
-
+  
   $log_name = "/var/log/remote/${node}/*"
   $log_tag  = "${node}"
 
